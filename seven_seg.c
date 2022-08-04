c73c1601905b201786b9e6c24683334e5306ee5c
Muhd Syahir <96367612+TheRealSyahir@users.noreply.github.com>
2022-08-04 20:58:30 +0800

    Update seven_seg.c

--- seven_seg.c
+++ seven_seg.c
@@ -15,9 +15,7 @@
 void initLCD(void);
 void lcdWriteData(char);
 
-
 extern unsigned char dig1, dig0; // removed dig2, dig3
-
 
 // The keyword, const will locate the initialised array in the program
 // flash memory. This enables RAM space to be saved up.
@@ -25,51 +23,63 @@ extern unsigned char dig1, dig0; // removed dig2, dig3
 // modify
 const unsigned char segTable[7] =
 {   
+    //0b11000000, // O
+    0b10001001, // H
+    0b11000000, // N
     0b11000111, // L
-    0b11000000, // O
-    0b10001001, // K
-    0b10001011, // h
-    0b11001111, // I
+    
     0b11111111  // Blank  
 };
+
 
 void disp7seg(void) // Function definition
 {
     PORTAbits.RA2 = 0; // Set RA<2:1> = 00
     PORTAbits.RA1 = 0;
     PORTD = segTable[dig0]; // display on rightmost digit of display
-
+    /*
     PORTAbits.RA2 = 0; // Set RA<2:1> = 01 
     PORTAbits.RA1 = 1;
     PORTD = segTable[dig1]; // display on rightmost digit of display
+    */
 }
 
 void lowTemp(void){
+    unsigned int cnt;
     PORTA = 0b00010000;
-    dig0 = 0;
-    dig1 = 1;
+    dig0 = 2;
+    //dig1 = 1;
     disp7seg();
+    for (cnt = 0; cnt<500; cnt++)
+    {
+        SPEAKER = 1;
+        __delay_us(100);
+        SPEAKER = 0;
+        __delay_us(100);
+    }
 }
 
 void normalTemp(void){
-    unsigned char i;
-    char normTemp[] = "TEMP: NORMAL";
     PORTA = 0b00100000;
     dig0 = 1;
-    dig1 = 2;
+    //dig1 = 2;
     disp7seg();
-    
-    for(i=0; normTemp[i]!=0; i++)
-    {
-        lcdWriteData(normTemp[i]);
-    }
     
 }
 
+
 void highTemp(void){
+    unsigned int cnt;
     PORTA = 0b00001000;
-    dig0 = 3;
-    dig1 = 4;
+    dig0 = 0;
+    //dig1 = 4;
     disp7seg();
+    for (cnt = 0; cnt<2000; cnt++)
+    {
+        SPEAKER = 1;
+        __delay_us(250);
+        SPEAKER = 0;
+        __delay_us(250);
+    }
 }
 
